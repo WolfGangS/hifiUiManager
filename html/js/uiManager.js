202ac697f199444bacc46ef3eb1d1fa0f32da3b7
BuildTools <unconfigured@null.spigotmc.org>
2017-01-22 19:28:09 +0000

    debug

--- html/js/uiManager.js
+++ html/js/uiManager.js
@@ -25,10 +25,11 @@ function testObj(obj,head){
 
 function getRepoListings(){
     alert("REPOS");
+    return;
     $.ajax({
         url: "repos.json",
         success: function(data){
-            alert(data);
+            //alert(data);
             webEvent({command:"setRepoList",value:JSON.parse(data)});
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
