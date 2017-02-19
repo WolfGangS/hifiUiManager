4e45af4bb141d4a665e9b6ea97275db86779f6d0
BuildTools <unconfigured@null.spigotmc.org>
2017-02-19 13:36:56 +0000

    Added cache break to uiManager.js

--- uiManager.js
+++ uiManager.js
@@ -1,4 +1,5 @@
 //This file is mearly a stub now as the main script has been renamed.
 //It exists only to not break the system for users still using the old version.
 //Please use the Main script, HiFiUiManager.js instead.
-Script.include("HiFiUiManager.js");
+//This function also now includes a Cache break to try and help developers
+Script.include("HiFiUiManager.js?" + Math.floor(Date.now() / 1000));
