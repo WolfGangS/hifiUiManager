b1c4c767b581630b98c66e6026408c4b3a6301c9
BuildTools <unconfigured@null.spigotmc.org>
2017-02-19 13:37:17 +0000

    Push

--- uiManager.js
+++ uiManager.js
@@ -1,5 +1,5 @@
 //This file is mearly a stub now as the main script has been renamed.
 //It exists only to not break the system for users still using the old version.
 //Please use the Main script, HiFiUiManager.js instead.
-//This function also now includes a Cache break to try and help developers
+//This function also now includes a cache break to try and help developers
 Script.include("HiFiUiManager.js?" + Math.floor(Date.now() / 1000));
